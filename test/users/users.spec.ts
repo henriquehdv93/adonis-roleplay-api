0fc7841bc6043709f1367bf931fcda5c78fab1d2
Henrique Vargas <henriquehdv93@gmail.com>
2023-06-21 19:53:04 -0300

    added database and user unit testing

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -3,12 +3,19 @@ import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 test.group('User', () => {
-  test.only('it should create an user', async () => {
+  test.only('it should create an user', async (assert) => {
     const userPayload = {
       email: 'henriquehdv93@gmail.com',
       username: 'Henrique',
       password: '123456',
+      avatar: 'http://images.com/image/1.png',
     }
-    await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    assert.exists(body.user, 'User undefined')
+    assert.exists(body.user.id, 'Id undefined')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.password, userPayload.password)
+    assert.equal(body.user.avatar, userPayload.avatar)
   })
 })
